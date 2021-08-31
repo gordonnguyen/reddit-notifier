7bcddc5c749e67e7d7175f61dac963920a9accb5
Gordon Nguyen <maixanh45@yahoo.com>
2021-08-31 17:13:45 -0500

    Added new functions

---
+++ discord-bot - test.py
@@ -0,0 +1,68 @@
+from discord.ext import tasks
+import discord
+import time
+import asyncio
+import asyncpraw
+'''
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('$dit'):
+        await message.channel.send('Dit me may tg cho!')
+
+@client.event
+async def new_message(new):
+    await new.channel.send('Fuck You')
+
+client.run('ODI0Nzk0Mjk2MTE2NzA3MzI4.YF0jjA.qMyUh6TstVUuD2fMPyNPX2VNXv0')
+'''
+
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # create the background task and run it in the background
+        self.reddit = asyncpraw.Reddit(
+            client_id="oK3dTEYR7exUog",
+            client_secret="D2sVNa17qetOcmp35iA4LaPwec2Qkg",
+            user_agent="MyBot/0.1"
+        )
+        self.bg_task = self.loop.create_task(self.notify_subreddit("buildapcsales"))
+
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def notify_subreddit(self, subreddit_name):
+        await self.wait_until_ready()
+        channel = self.get_channel(877911044931092491) # channel ID goes here
+        subreddit = await self.reddit.subreddit(subreddit_name)
+        async for submission in subreddit.stream.submissions():
+            send_embed = discord.Embed(
+                title=submission.title, 
+                url=submission.url, 
+                description=submission.permalink
+                )
+            send_embed.set_thumbnail("https://via.placeholder.com/250")
+            print("set successfully")
+            #send_message = submission.title + "\n" + submission.url + "\n"
+            await channel.send(embed=send_embed)
+            print('Sent message')
+            await asyncio.sleep(5)
+
+client = MyClient()
+client.run('ODI0Nzk0Mjk2MTE2NzA3MzI4.YF0jjA.qMyUh6TstVUuD2fMPyNPX2VNXv0')
